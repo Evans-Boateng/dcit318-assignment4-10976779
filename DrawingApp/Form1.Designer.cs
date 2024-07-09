1e037bbdacfc412cfcd52819f614bb5898745ef6
Evans Boateng <eb5337877@gmail.com>
2024-07-09 13:10:18 -0700

    Implemented basic drawing functionality using mouse events.

---
+++ DrawingApp/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCanvas.Location = new System.Drawing.Point(0, 0);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(800, 450);
+            this.panelCanvas.TabIndex = 0;
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelCanvas);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCanvas;
+    }
+}
+
---
+++ DrawingApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint = Point.Empty; // Store the last point for drawing lines
+        private Bitmap canvasBitmap; // Bitmap to store the drawings
+        private Graphics canvasGraphics;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCanvas();
+        }
+
+        private void InitializeCanvas()
+        {
+            // Initialize the bitmap and graphics object
+            canvasBitmap = new Bitmap(panelCanvas.Width, panelCanvas.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.Clear(Color.White); // Set the initial background to white
+            panelCanvas.BackgroundImage = canvasBitmap; // Set the panel's background to the bitmap
+        }
+
+        private void panelCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                if (lastPoint != Point.Empty)
+                {
+                    canvasGraphics.DrawLine(Pens.Black, lastPoint, e.Location);
+                    lastPoint = e.Location;
+                    panelCanvas.Invalidate(); // Refresh the panel to show the new drawing
+                }
+            }
+        }
+        private void panelCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+                lastPoint = Point.Empty;
+            }
+        }
+        private void panelCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
