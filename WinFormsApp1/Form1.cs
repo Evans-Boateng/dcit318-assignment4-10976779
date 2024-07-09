9b86b53a0635f681d632fe787a767de1838c25ab
Evans Boateng <eb5337877@gmail.com>
2024-07-09 11:10:06 -0700

    This is the login screen form

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String username = textBox1.Text;
+            String password = textBox2.Text;
+            MessageBox.Show("Your username is " + username + " and your password is " + password);
+        }
+    }
+}
